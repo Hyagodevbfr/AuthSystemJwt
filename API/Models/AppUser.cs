c6af7fdd96f8e8bc3e9c01488d20aa1c2e00294e
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-17 12:27:52 -0300

    Update AppUser

--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
-        
+        public string? FullName { get; set; }
     }
 }
