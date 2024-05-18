e0e32ed0ee5c1865ac6a92a0b8f85c2a658fcb83
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-18 08:56:25 -0300

    Install Font from Google Fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        poppins:["Poppins", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
