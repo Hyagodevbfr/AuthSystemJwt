592ca9d14abfe78566136261aad85132cf81e361
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-20 11:02:58 -0300

    Updates

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021c4a11ef386810ed30e662ddcf795dd7520aca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a31f6214b0faa7a60a3d0c5b8952afa5d98f61")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
