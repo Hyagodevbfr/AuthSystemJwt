a3a12831b476fb2152165d514c8f3e1279a078eb
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-20 06:08:12 -0300

    Create login page

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule,MatIconModule,RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  hide = true;
+  form!:FormGroup;
+  fb = inject(FormBuilder);
+  Login(){}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email:['', Validators.required, Validators.email],
+      password:['',Validators.required]
+    });
+  }
 }
