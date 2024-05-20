6451147df7cc63e965d851a0a11375e497d09ffe
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-20 08:51:00 -0300

    Configure login and logout navbar

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -4,17 +4,18 @@ import { LoginRequest } from '../interfaces/login-request';
 import { Observable, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { AuthResponse } from '../interfaces/auth-response';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  
   apiUrl: string = environment.apiUrl;
   private tokenKey ='token'
-
+  
   constructor(private http:HttpClient) { }
-
+  
   Login(data:LoginRequest): Observable<AuthResponse>{
     return this.http.post<AuthResponse>(`${this.apiUrl}account/login`, data).pipe(
       map((response)=>{
@@ -25,4 +26,39 @@ export class AuthService {
       })
     );
   }
+
+  getUserDetail=()=>{
+    const token = this.getToken();
+    if(!token) return null;
+    const decodedToken:any = jwtDecode(token);
+    const userDetail ={
+      id:decodedToken.nameid,
+      fullName:decodedToken.name,
+      email:decodedToken.email,
+      roles:decodedToken.role || []
+    }
+    return userDetail;
+  }
+
+  isLoggedIn=():boolean=>{
+    const token = this.getToken();
+    if(!token) return false;
+    
+    return !this.isTokenExpired();
+  }
+
+  private isTokenExpired() {
+    const token = this.getToken();
+    if(!token) return true;
+    const decoded = jwtDecode(token);
+    const isTokenExpired = Date.now() >= decoded['exp']! * 1000;
+    if(isTokenExpired) this.logout();
+    return isTokenExpired;
+  }
+
+  logout=(): void=>{
+    localStorage.removeItem(this.tokenKey)
+  }
+  
+  private getToken =():string | null => localStorage.getItem(this.tokenKey) ||'';
 }
