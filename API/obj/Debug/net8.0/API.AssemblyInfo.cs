5c3a218260e0f110a7f6a818da9dd71ae45a5839
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-21 16:10:21 -0300

    Complement reset password component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3940fd8e601fb78786443863e511b02db6ac8da8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e1cf67d4064292195497ee18b87b27ffdd8afc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
