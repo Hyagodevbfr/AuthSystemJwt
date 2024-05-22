c70f5e85ef3ecd3ed81577ae5bb355d6d085d611
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-21 21:10:23 -0300

    Implement forgot password

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e1cf67d4064292195497ee18b87b27ffdd8afc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72525bb48c81383d0378ff3bf7969d4ac88fb39b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
